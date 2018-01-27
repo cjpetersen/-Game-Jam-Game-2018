f08b38c2417d5d2288c338fdb0ce3300f13d8e80
Nic Brosz <nbrosz@ibrewcode.com>
2018-01-27 01:48:13 -0600

    Add scripts for player movement and looking

---
+++ Stealthy Liberation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public enum MoveSpeed
+    {
+        Walk,
+        Run
+    }
+
+    public MoveSpeed moveSpeed;
+    public float runSpeed;
+    public float walkSpeed;
+
+    CharacterController characterController;
+
+    public float CurrentMoveSpeed {
+        get {
+            return moveSpeed == MoveSpeed.Run ? runSpeed : walkSpeed;
+        }
+    }
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    
+    void Start () {
+		
+	}
+	
+	void Update () {
+        var zMove = 0f;
+        var xMove = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            zMove = 1;
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            zMove = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            xMove = 1;
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            xMove = -1;
+        }
+
+        characterController.SimpleMove(new Vector3(xMove, 0, zMove));
+	}
+}
