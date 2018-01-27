1ab0eb66c33909268416f4b172fcfbe20fcae4d8
Carl Petersen <carl.petersen@trojans.dsu.edu.com>
2018-01-27 15:44:26 -0600

    NavMesh changes

---
+++ Stealthy Liberation/Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTowards : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+    //public GameObject target;
+    private NavMeshPath navMeshPath;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        //navMeshAgent.CalculatePath(target.transform.position, navMeshPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+
+    }
+}
