d037e372ce8b9640312c08149af39a573f5a4fb4
Carl Petersen <carl.petersen@trojans.dsu.edu.com>
2018-01-27 19:23:42 -0600

    Enemy finds player gives up after 5 seconds

---
+++ Stealthy Liberation/Assets/Scripts/FollowScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowScript : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public Transform target;
+    private bool playerFound = false;
+    public float LostPlayerTimer = 5f;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    void Update()
+    {
+        if (playerFound == true)
+        {
+            LostPlayerTimer -= Time.deltaTime;
+        }
+        
+        if (LostPlayerTimer <= 0)
+        {
+            playerFound = false;
+        }
+
+        if (Vector3.Distance(target.position, transform.position) < 5)
+        {
+            playerFound = true;
+
+            agent.SetDestination(target.position);
+
+                LostPlayerTimer = 5f;
+              
+        }
+
+
+
+        
+
+
+
+    }
+}
