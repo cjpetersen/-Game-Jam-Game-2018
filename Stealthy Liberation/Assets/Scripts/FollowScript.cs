775ff205fceb65ddcc657d94ebb93b79bed82d33
Carl Petersen <carl.petersen@trojans.dsu.edu.com>
2018-01-27 21:27:11 -0600

    stuff

--- Stealthy Liberation/Assets/Scripts/FollowScript.cs
+++ Stealthy Liberation/Assets/Scripts/FollowScript.cs
@@ -8,13 +8,11 @@ public class FollowScript : MonoBehaviour
 
     NavMeshAgent agent;
     public Transform target;
-    public Transform target2;
-    public Transform target3;
+    public Transform[] targetArr;
     private bool playerFound = false;
     public float LostPlayerTimer = 5f;
     public float PatrolTimer = 5f;
-    public float PatrolTimer2 = 7f;
-    public float PatrolTimer3 = 7f;
+    public int PatrolIndex = 0;
 
     void Start()
     {
@@ -27,18 +25,14 @@ public class FollowScript : MonoBehaviour
         if (playerFound == true)
         {
             LostPlayerTimer -= Time.deltaTime;
-            PatrolTimer = 7;
-            PatrolTimer2 = 7;
-            PatrolTimer3 = 7;
-
+            
         }
+        
         
         if (LostPlayerTimer <= 0)
         {
             playerFound = false;
-             PatrolTimer = 5;
-             PatrolTimer2 = 7;
-            PatrolTimer3 = 7;
+            
         }
 
         if (Vector3.Distance(target.position, transform.position) < 5)
@@ -50,32 +44,28 @@ public class FollowScript : MonoBehaviour
                 LostPlayerTimer = 5f;
               
         }
+        
         else
-        if (PatrolTimer < 6)
-        { PatrolTimer -= Time.deltaTime; }
-        if (PatrolTimer2 < 6)
-        { PatrolTimer2 -= Time.deltaTime; }
-
-
-
-        if (PatrolTimer <= 5)
         {
-            agent.SetDestination(target3.position);
+            PatrolTimer -= Time.deltaTime;
+        }
+        
+
+        if (PatrolTimer <= 5 && playerFound == false)
+        {
+            agent.SetDestination(targetArr[PatrolIndex].position);
             if (PatrolTimer <= 0)
             {
-                PatrolTimer2 = 5;
-                PatrolTimer = 6;
-            }
-        }
-        if (PatrolTimer2 <= 5)
-        {
-            agent.SetDestination(target2.position);
-            if (PatrolTimer2 <= 0)
-            {
+                PatrolIndex++;
                 PatrolTimer = 5;
-                PatrolTimer2 = 6;
+               
+                if (PatrolIndex == targetArr.Length)
+                {
+                    PatrolIndex = 0;
+                }
             }
         }
+   
     }
 
 }
