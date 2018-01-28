982f1a8223c77423d553a04450a3013d9872277f
Carl Petersen <carl.petersen@trojans.dsu.edu.com>
2018-01-27 21:43:41 -0600

    CharcterDeath

---
+++ Stealthy Liberation/Assets/Scripts/CharcterDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharcterDeath : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
