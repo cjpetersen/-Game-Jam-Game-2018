1f7d4e79543222995c928658dab5ac9d68510846
Nic Brosz <nbrosz@ibrewcode.com>
2018-01-28 12:18:15 -0600

    Add start of lights to Main scene

---
+++ Stealthy Liberation/Assets/Scripts/SoundArea.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundArea : MonoBehaviour {
+    [SerializeField]
+    private List<SoundArea> _attachedAreas = new List<SoundArea>();
+    private bool _initialized = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (!_initialized)
+            _initialized = true;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_initialized)
+        {
+            var otherSoundArea = other.GetComponent<SoundArea>();
+            if (otherSoundArea)
+            {
+                _attachedAreas.Add(otherSoundArea);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        foreach( var soundArea in _attachedAreas)
+        {
+            Gizmos.DrawLine(transform.position, soundArea.transform.position);
+        }
+    }
+}
