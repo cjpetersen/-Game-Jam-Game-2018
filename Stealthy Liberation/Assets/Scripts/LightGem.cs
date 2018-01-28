32ef189b7bf40a319b75827880434c49ca5290ef
Nic Brosz <nbrosz@ibrewcode.com>
2018-01-28 09:46:14 -0600

    Add on-screen light gem

---
+++ Stealthy Liberation/Assets/Scripts/LightGem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightGem : MonoBehaviour {
+
+    public Image canvasImage;
+    public Sprite partiallyHiddenSprite;
+    public Sprite visibleSprite;
+
+    private float latestHiddenLevel = 1;
+
+    private void Update()
+    {
+        // get hidden level
+        latestHiddenLevel = LightState.Instance.GetHideProbability(gameObject);
+    }
+
+    void OnGUI () {
+        // set light gem
+        if (latestHiddenLevel < .5)
+            canvasImage.overrideSprite = visibleSprite;
+        else if (latestHiddenLevel < 1)
+            canvasImage.overrideSprite = partiallyHiddenSprite;
+        else
+            canvasImage.overrideSprite = null;
+    }
+}
