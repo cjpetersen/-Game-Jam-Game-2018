1aa8be28ac9bbc840e8065e95b480ee61bd775fd
Carl Petersen <carl.petersen@trojans.dsu.edu.com>
2018-01-27 20:59:03 -0600

    Patroling

--- Stealthy Liberation/Assets/Scripts/FollowScript.cs
+++ Stealthy Liberation/Assets/Scripts/FollowScript.cs
@@ -8,8 +8,14 @@ public class FollowScript : MonoBehaviour
 
     NavMeshAgent agent;
     public Transform target;
+    public Transform target2;
+    public Transform target3;
     private bool playerFound = false;
     public float LostPlayerTimer = 5f;
+    public float PatrolTimer = 5f;
+    public float PatrolTimer2 = 7f;
+    public float PatrolTimer3 = 7f;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -21,11 +27,18 @@ public class FollowScript : MonoBehaviour
         if (playerFound == true)
         {
             LostPlayerTimer -= Time.deltaTime;
+            PatrolTimer = 7;
+            PatrolTimer2 = 7;
+            PatrolTimer3 = 7;
+
         }
         
         if (LostPlayerTimer <= 0)
         {
             playerFound = false;
+             PatrolTimer = 5;
+             PatrolTimer2 = 7;
+            PatrolTimer3 = 7;
         }
 
         if (Vector3.Distance(target.position, transform.position) < 5)
@@ -37,12 +50,33 @@ public class FollowScript : MonoBehaviour
                 LostPlayerTimer = 5f;
               
         }
+        else
+        if (PatrolTimer < 6)
+        { PatrolTimer -= Time.deltaTime; }
+        if (PatrolTimer2 < 6)
+        { PatrolTimer2 -= Time.deltaTime; }
 
 
 
-        
-
-
-
+        if (PatrolTimer <= 5)
+        {
+            agent.SetDestination(target3.position);
+            if (PatrolTimer <= 0)
+            {
+                PatrolTimer2 = 5;
+                PatrolTimer = 6;
+            }
+        }
+        if (PatrolTimer2 <= 5)
+        {
+            agent.SetDestination(target2.position);
+            if (PatrolTimer2 <= 0)
+            {
+                PatrolTimer = 5;
+                PatrolTimer2 = 6;
+            }
+        }
     }
+
 }
+
---
+++ Stealthy Liberation/Assets/Scripts/PatrolScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public float PatrolTimer = 5f;
+    public float PatrolTimer2 = 7f;
+    public Transform target;
+    public Transform target2;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (PatrolTimer == 5 || PatrolTimer2 == 5)
+        if(PatrolTimer < 6)
+        { PatrolTimer -= Time.deltaTime; }
+        if (PatrolTimer2 < 6)
+        { PatrolTimer2 -= Time.deltaTime; }
+
+        
+        
+        if (PatrolTimer <= 5)
+        {
+            agent.SetDestination(target2.position);
+            if (PatrolTimer <= 0)
+            {
+                PatrolTimer2 = 5;
+                PatrolTimer = 6;
+            }
+        }
+        if (PatrolTimer2 <= 5)
+        {
+            agent.SetDestination(target.position);
+            if (PatrolTimer2 <= 0)
+            {
+                PatrolTimer = 5;
+                PatrolTimer2 = 6;
+            }
+        }
+    }
+}
